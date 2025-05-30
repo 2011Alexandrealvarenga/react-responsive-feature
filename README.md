345bbb03c4615717abb2ed9d638bd59ab2085c89
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2025-05-29 21:47:32 -0300

    flexbox

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## Repository
 https://github.com/2011Alexandrealvarenga/ReactJs-Exercises.git
 
+## Tecnologies
+React, tailwindcss 
+
 ## Execute project
 npm run dev
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Container } from "./features/container";
+import { Flexbox } from "./features/flexBox";
 import { Responsivite } from "./features/responsivite";
 
 
 export default function Home() {
   return (
     <>
-      <Responsivite />
-    </>
-
-   
+      <Flexbox/>
+    </>   
   );
 }
