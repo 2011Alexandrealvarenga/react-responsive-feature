d1a19f1a6673179ce34080eb42cc9115c25b6310
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2025-05-30 10:26:22 -0300

    grid

---
+++ src/app/features/grid.tsx
@@ -0,0 +1,19 @@
+export const Grid = () =>{
+  return(
+    <>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-2">
+          <div className="cell col-span-full">01</div>
+          <div className="cell col-span-2">02</div>
+          <div className="cell">03</div>
+          <div className="cell">04</div>
+          <div className="cell col-start-2 col-end-4">05</div>
+          <div className="cell row-start-2">06</div>
+          <div className="cell">07</div>
+          <div className="cell">08</div>
+          <div className="cell">09</div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Container } from "./features/container";
 import { Flexbox } from "./features/flexBox";
+import { Grid } from "./features/grid";
 import { Responsivite } from "./features/responsivite";
 
 
 export default function Home() {
   return (
     <>
-      <Flexbox/>
+      <Grid/>
     </>   
   );
 }
